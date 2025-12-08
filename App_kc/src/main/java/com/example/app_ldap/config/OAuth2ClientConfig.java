cd3b4ed1f193616e84de8db109dfa52ac21dd7b1
username / Gio <falcogiovanni01@gmail.com>
2025-12-08 16:24:59 +0100

    ora funziona tutto c'erano dei piccoli errori di configurazione

--- App_kc/src/main/java/com/example/app_ldap/config/OAuth2ClientConfig.java
+++ App_kc/src/main/java/com/example/app_ldap/config/OAuth2ClientConfig.java
@@ -17,23 +17,43 @@ public class OAuth2ClientConfig {
     @Value("${spring.security.oauth2.client.registration.keycloak.client-secret}")
     private String clientSecret;
 
-    @Bean
+    @Value("${keycloak.auth-uri}")
+    private String authUri;
+
+    @Value("${keycloak.token-uri}")
+    private String tokenUri;
+
+    @Value("${keycloak.user-info-uri}")
+    private String userInfoUri;
+
+    @Value("${keycloak.jwk-set-uri}")
+    private String jwkSetUri;
+
+
+
+  @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration keycloakRegistration = ClientRegistration
-                .withRegistrationId("keycloak")
+        ClientRegistration registration = ClientRegistration.withRegistrationId("keycloak")
                 .clientId(clientId)
                 .clientSecret(clientSecret)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
                 .scope("openid", "profile", "email", "roles")
-                .authorizationUri("http://localhost/realms/mensa/protocol/openid-connect/auth")
-                .tokenUri("http://keycloak:8080/realms/mensa/protocol/openid-connect/token")
-                .userInfoUri("http://keycloak:8080/realms/mensa/protocol/openid-connect/userinfo")
-                .jwkSetUri("http://keycloak:8080/realms/mensa/protocol/openid-connect/certs")
+                
+               
+                // Browser -> HTTPS Localhost (tramite Nginx-User)
+                .authorizationUri(authUri)
+                
+                // Backend -> HTTP Interno Docker (tramite porta 8080 diretta)
+                .tokenUri(tokenUri)
+                .userInfoUri(userInfoUri)
+                .jwkSetUri(jwkSetUri)
+                // -----------------------------
+                
                 .userNameAttributeName("preferred_username")
                 .clientName("Keycloak")
                 .build();
 
-        return new InMemoryClientRegistrationRepository(keycloakRegistration);
+        return new InMemoryClientRegistrationRepository(registration);
     }
 }
